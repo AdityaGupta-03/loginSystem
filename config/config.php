136a4f6b9ed3bb386ffb0d6a6d26e799b9146f98
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-04-20 09:12:53 +0530

    Defining Base URL of the site

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_URL', 'http://localhost/Addy/loginSystem/');
+// Define the base URL of your website
+// require("config/config.php");  this will be require everytime before using BASE_URL constant;
+// echo BASE_URL = this is used for image src in navbar which will have different src paths depending upon file used.
+
+?>
